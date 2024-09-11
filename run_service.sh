868aded804967f2e46f3f1d5f4ca9e704e7f22c9
David Vilela <dvilelaf@gmail.com>
2024-09-11 16:17:15 +0200

    fix: add run_service script

---
+++ run_service.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+REPO_PATH=$PWD
+
+# Remove previous service build
+if test -d hello_world; then
+  echo "Removing previous service build"
+  sudo rm -r hello_world
+fi
+
+# Push packages and fetch service
+make clean
+
+autonomy push-all
+
+autonomy fetch --local --service valory/hello_world && cd hello_world
+
+# Build the image
+autonomy init --reset --author valory --remote --ipfs --ipfs-node "/dns/registry.autonolas.tech/tcp/443/https"
+autonomy build-image
+
+# Copy .env file
+cp $REPO_PATH/.env .
+
+# Copy the keys and build the deployment
+cp $REPO_PATH/keys.json .
+
+autonomy deploy build -ltm
+
+# Run the deployment
+autonomy deploy run --build-dir abci_build/
